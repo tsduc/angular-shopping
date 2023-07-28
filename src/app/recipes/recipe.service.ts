40eafeacd7278ad59f05bb11700a1a0e2b85e49a
tsduc <tsduc0996@gmail.com>
2023-07-28 15:16:56 +0700

    commit

---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,31 @@
+import { EventEmitter } from "@angular/core";
+import { Recipe } from "./recipe.model";
+import { Ingredient } from "../shared/ingredient.model";
+
+export class RecipeService{
+
+    recipeSelected = new EventEmitter<Recipe>();
+
+    private recipes: Recipe[] = [
+        new Recipe(
+            'A Test Recipe', 
+            'This is simply a test', 
+            'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg',
+            [
+                new Ingredient('Meat', 1),
+                new Ingredient('French Fries', 20)
+            ]),
+        new Recipe(
+            'Another Test Recipe', 
+            'This is simply a test', 
+            'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg',
+            [
+                new Ingredient('Buns', 2),
+                new Ingredient('Meat', 1)
+            ])
+    ];
+
+    getRecipes(){
+        return this.recipes.slice();
+    }
+}
